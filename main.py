d4e78ec624aeca9f53aef21df21118c7a4ceab6e
Jean Lucas De Cesare <jean.decesare21@gmail.com>
2023-06-16 10:49:22 -0300

    Comecei o projeto

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+tamanho = (800,600)
+branco = (255,255,255)
+tela =  pygame.display.set_mode( tamanho )
+running = True
+while running:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+pygame.quit()
