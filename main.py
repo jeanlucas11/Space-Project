069cd9b4c3b0bb03e6c3353efe277d643ac3df3f
Jean Lucas De Cesare <jean.decesare21@gmail.com>
2023-06-16 11:04:13 -0300

    Coloquei fundo e áudio

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
+
 pygame.init()
-tamanho = (800,600)
+
+tamanho = (800,500)
 branco = (255,255,255)
 tela =  pygame.display.set_mode( tamanho )
 running = True
 fonte = pygame.font.Font(None, 26)
-
+fundo = pygame.image.load("fundo.jpg")
+pygame.mixer.music.load("audio.mp3")
+pygame.mixer.music.play(-1)
 while running:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             running = False
-
+    tela.blit(fundo, (0,0))
     texto1 = fonte.render("Pressione F10 para Salvar os Pontos: ",True, branco )
     texto2 = fonte.render("Pressione F11 para Carregar os Pontos: ",True, branco )
     texto3 = fonte.render("Pressione F12 para Deletar os Pontos: ",True, branco )
     tela.blit(texto1, (10,10))
     tela.blit(texto2, (10,30))
     tela.blit(texto2, (10,50))
+
     pygame.display.update()
 pygame.quit()
