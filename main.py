4b11bce0fbf012b32f456a4a8a8664e074edc323
Jean Lucas De Cesare <jean.decesare21@gmail.com>
2023-06-16 10:56:54 -0300

    Coloquei a escrita

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ tamanho = (800,600)
 branco = (255,255,255)
 tela =  pygame.display.set_mode( tamanho )
 running = True
+fonte = pygame.font.Font(None, 26)
+
 while running:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             running = False
 
+    texto1 = fonte.render("Pressione F10 para Salvar os Pontos: ",True, branco )
+    texto2 = fonte.render("Pressione F11 para Carregar os Pontos: ",True, branco )
+    texto3 = fonte.render("Pressione F12 para Deletar os Pontos: ",True, branco )
+    tela.blit(texto1, (10,10))
+    tela.blit(texto2, (10,30))
+    tela.blit(texto2, (10,50))
     pygame.display.update()
 pygame.quit()
